664899ed0e18b4b46cad7151f5f71141cff9e31b
some0necoding <codingandprogramming.code@gmail.com>
2023-03-01 09:06:59 +0100

    creating install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#		---------------- install.sh ----------------
+#
+#		This script installs settheme utility.
+#
+
+# TODO: check DE and packages
+# TODO: put units in /home/${USER}/.config/systemd/user/
+# TODO: put commands in /usr/local/bin
