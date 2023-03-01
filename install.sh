8ca7fbd393916a7298ee4b37ad2c8cde22866b85
some0necoding <codingandprogramming.code@gmail.com>
2023-03-01 09:14:39 +0100

    adding check_de function to check desktop environment

--- install.sh
+++ install.sh
@@ -5,6 +5,29 @@
 #		This script installs settheme utility.
 #
 
-# TODO: check DE and packages
+# TODO: check packages
 # TODO: put units in /home/${USER}/.config/systemd/user/
 # TODO: put commands in /usr/local/bin
+
+# Check desktop environment
+check_de() {
+
+	[[ $XDG_CURRENT_DESKTOP != "KDE" || $DESKTOP_SESSION != "plasma" ]] && {
+		echo "settheme works only on KDE plasma desktop"
+		return 1
+	}
+
+	return 0
+}
+
+check_packages() {
+
+}
+
+install_cmds() {
+
+}
+
+install_units() {
+
+}
